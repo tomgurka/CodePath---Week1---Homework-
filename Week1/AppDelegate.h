5181f4a8b50a7e15543d99b44df281ec93315383
Tom Gurka <tommy.gurka@gmail.com>
2014-06-06 19:41:31 -0700

    Initial Commit

---
+++ Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week1
+//
+//  Created by Tom Gurka on 6/6/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
